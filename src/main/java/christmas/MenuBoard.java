bde1a96cbe8e7c3eda1b729c6708e5dc2f5e85bb
kwon <kckc0608@naver.com>
2023-11-15 12:04:15 +0900

    feat: 없는 메뉴 요청시 에러 발생 로직 추가

--- src/main/java/christmas/MenuBoard.java
+++ src/main/java/christmas/MenuBoard.java
@@ -6,7 +6,12 @@ public class MenuBoard {
     private static HashMap<String, Menu> menuBoard;
 
     public static Menu getMenuByName(String name) {
-        return menuBoard.get(name);
+        Menu menu  = menuBoard.get(name);
+        if (menu == null) {
+            // TODO : Print Error Message
+            throw new IllegalArgumentException();
+        }
+        return menu;
     }
 
     public static void addNewMenu(String name, Menu menu) {
