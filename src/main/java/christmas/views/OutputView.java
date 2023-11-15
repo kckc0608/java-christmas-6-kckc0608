53c45e68e8829a1827ac9695a13d384f1e7b43f1
kwon <kckc0608@naver.com>
2023-11-15 14:05:05 +0900

    feat: 이벤트 혜택 미리 보기 안내 문구 출력

--- src/main/java/christmas/views/OutputView.java
+++ src/main/java/christmas/views/OutputView.java
@@ -1,14 +1,20 @@
 package christmas.views;
 
+import christmas.Order;
+
 public class OutputView {
     private final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
 
     public void printWelcome() {
         System.out.println(WELCOME_MESSAGE);
     }
-    public void printMenu() {
+
+    public void printBenefitsInfo() {
+        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+    }
+    public void printMenu(Order order) {
         System.out.println("<주문 메뉴>");
-        // ...
+        // order.printOrder();
     }
     // ...
 }
