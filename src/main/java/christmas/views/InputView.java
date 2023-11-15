777ef3779aa9d3fe311767a09a5e3a79c6baa795
kwon <kckc0608@naver.com>
2023-11-15 11:38:02 +0900

    feat: read Order Input View 작성

--- src/main/java/christmas/views/InputView.java
+++ src/main/java/christmas/views/InputView.java
@@ -1,5 +1,10 @@
 package christmas.views;
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Menu;
+import christmas.Order;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputView {
     public int readDate() {
@@ -9,4 +14,15 @@ public class InputView {
         return Integer.parseInt(input);
     }
 
+    public Order readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        Map<Menu, Integer> orderMap = validateAndConvertInputToOrderMap(input);
+        return new Order(orderMap);
+    }
+
+    private Map<Menu,Integer> validateAndConvertInputToOrderMap(String orderInput) {
+        // TODO: Implement
+        return new HashMap<Menu, Integer>();
+    }
 }
