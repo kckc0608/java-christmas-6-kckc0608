c3fd5a000022277de3581de3ccab3667ad29f974
kwon <kckc0608@naver.com>
2023-11-15 19:06:41 +0900

    feat: 크리스마스 디데이 할인 이벤트 적용 기간 체크

--- src/main/java/christmas/Event.java
+++ src/main/java/christmas/Event.java
@@ -2,8 +2,39 @@ package christmas;
 
 public class Event {
     private Order order;
+    private int orderDate;
 
-    public Event(Order order) {
+    private int saleAmount = 0;
+    private int giftAmount = 0;
+
+    public Event(Order order, int orderDate) {
         this.order = order;
+        this.orderDate = orderDate;
+    }
+
+    public void applyEvent() {
+        if (1 <= orderDate && orderDate <= 25) {
+            applyChristmasDDaySaleEvent();
+        }
+    }
+
+    private void applyChristmasDDaySaleEvent() {
+
+    }
+
+    private void applyWeekDaySaleEvent() {
+
+    }
+
+    private void applyWeekendSaleEvent() {
+
+    }
+
+    private void applySpecialSaleEvent() {
+
+    }
+
+    private void applyGiftEvent() {
+
     }
 }
